376b2e195c476345f1b95d15296fba49a1cbfb7f
08ai <08ai@users.noreply.github.com>
2026-06-15 20:32:32 +0800

    fix: back to keyWindow + 1s bringToFront

--- TikTokHelper.m
+++ TikTokHelper.m
@@ -141,16 +141,11 @@ static void hooked_setLastMsg(id self, SEL _cmd, id message) {
 }
 
 - (void)buildUI {
-    gWin=keyWin(); if(!gWin){dispatch_after(dispatch_time(DISPATCH_TIME_NOW,2*NSEC_PER_SEC),dispatch_get_main_queue(),^{[self buildUI];});return;}
-    // 把按钮加到 TikTok 内容层里面，确保能接收触摸
-    UIView *contentView = gWin;
-    if (gWin.subviews.count > 0) {
-        UIView *tv = gWin.subviews[0]; // UITransitionView
-        contentView = tv.subviews.count > 0 ? tv.subviews.lastObject : tv;
-    }
+    gWin=keyWin(); if(!gWin){dispatch_after(dispatch_time(DISPATCH_TIME_NOW,3*NSEC_PER_SEC),dispatch_get_main_queue(),^{[self buildUI];});return;}
     CGFloat SW=[UIScreen mainScreen].bounds.size.width;
     gToggleBtn=[self makeBtn:@"展开" frame:CGRectMake(SW-95,120,85,48) bg:rgb(.92,.1,.1,.92) fs:18];
-    gToggleBtn.layer.cornerRadius=16;[gToggleBtn addTarget:self action:@selector(onToggle) forControlEvents:1<<6];[contentView addSubview:gToggleBtn];
+    gToggleBtn.layer.cornerRadius=16;[gToggleBtn addTarget:self action:@selector(onToggle) forControlEvents:UIControlEventTouchUpInside];
+    [gWin addSubview:gToggleBtn];
     CGFloat pW=175,pH=240;
     gPanel=[[UIView alloc]initWithFrame:CGRectMake(100,70,pW,pH)];
     gPanel.backgroundColor=rgb(1,.85,.02,.95);gPanel.layer.cornerRadius=14;
@@ -171,9 +166,9 @@ __attribute__((constructor)) static void THInit(void) {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW,3*NSEC_PER_SEC),dispatch_get_main_queue(),^{
         [TikTokHelper installHook];
         [[[TikTokHelper alloc] init] buildUI];
-        [NSTimer scheduledTimerWithTimeInterval:2 repeats:YES block:^(NSTimer*t){
-            if(gToggleBtn)[gWin bringSubviewToFront:gToggleBtn];
-            if(gPanel&&gExpanded)[gWin bringSubviewToFront:gPanel];
+        [NSTimer scheduledTimerWithTimeInterval:1.0 repeats:YES block:^(NSTimer*t){
+            if(gToggleBtn&&gWin)[gWin bringSubviewToFront:gToggleBtn];
+            if(gPanel&&gExpanded&&gWin)[gWin bringSubviewToFront:gPanel];
         }];
         LOG(@"注入完成");
     });

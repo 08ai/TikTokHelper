3e963a4946ff03cc3d39ff49bfac3471ea6ebd0a
08ai <08ai@users.noreply.github.com>
2026-06-18 15:17:47 +0800

    fix: use IESIMConversationDataManager.createConversation to properly create conversations

--- TikTokHelper.m
+++ TikTokHelper.m
@@ -249,17 +249,43 @@ static void hooked_setLastMsg(id self, SEL _cmd, id message) {
                 setStatus([NSString stringWithFormat:@"群发 %ld/%lu: %@", (long)(i+1), (unsigned long)uids.count, uid]);
             });
             @try {
-                Class ConvUtil = NSClassFromString(@"AWEIMCoreConversationUtil");
-                id msgConv = ((id(*)(id,SEL,id,id))objc_msgSend)(ConvUtil, NSSelectorFromString(@"getConversationWithID:options:"), uid, nil);
-                // AWEIMMessageConversation.con → TIMOConversation (用 KVC 读取)
-                id timoConv = [msgConv valueForKey:@"con"];
-                LOG(@"Batch [%ld] uid=%@ msg=%@ timo=%@", (long)i, uid, [msgConv class], [timoConv class]);
-                if (timoConv) {
-                    dispatch_async(dispatch_get_main_queue(), ^{
-                        [self sendViaTIMOCtrl:timoConv text:@"你好啊！在干嘛"];
-                    });
-                } else {
-                    LOG(@"Batch [%ld] timo nil", (long)i);
+                // 获取 conversationDataManager
+                Class PTM = NSClassFromString(@"AWEIMNewChatDetailPageTransitionManager");
+                id convDM = nil;
+                if (PTM) {
+                    convDM = ((id(*)(id,SEL))objc_msgSend)(PTM, NSSelectorFromString(@"conversationDataManager"));
+                }
+                LOG(@"Batch [%ld] uid=%@ convDM=%@", (long)i, uid, convDM);
+
+                if (convDM) {
+                    // 使用信号量等待异步回调
+                    dispatch_semaphore_t sem = dispatch_semaphore_create(0);
+                    __block id createdConv = nil;
+                    long long uidVal = [uid longLongValue];
+
+                    SEL createSel = NSSelectorFromString(@"createConversationWithOtherParticipants:type:inInbox:completion:");
+                    NSNumber *typeNum = @(1); // 1 = 1-on-1 DM
+                    NSNumber *inboxNum = @(0);
+                    void (^cb)(id) = ^(id conv) {
+                        createdConv = conv;
+                        dispatch_semaphore_signal(sem);
+                    };
+
+                    ((void(*)(id,SEL,id,unsigned long long,int,void(^)(id)))objc_msgSend)(
+                        convDM, createSel,
+                        @[uid], 1, 0, cb
+                    );
+
+                    dispatch_semaphore_wait(sem, dispatch_time(DISPATCH_TIME_NOW, 5*NSEC_PER_SEC));
+                    LOG(@"Batch [%ld] created=%@", (long)i, [createdConv class]);
+
+                    if (createdConv) {
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            [self sendViaTIMOCtrl:createdConv text:@"你好啊！在干嘛"];
+                        });
+                    } else {
+                        LOG(@"Batch [%ld] create failed", (long)i);
+                    }
                 }
             } @catch (NSException *e) {
                 LOG(@"Batch [%ld] crash: %@", (long)i, e);
